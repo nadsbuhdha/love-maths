31cbd28766f217e718e6fdaccf1cf6e307c4f778
nadsbuhdha <nadsbuhdha@gmail.com>
2021-11-06 16:54:01 +0000

    set up struction of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+function rungame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function increcemntScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
