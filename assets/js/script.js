8e4fd1d7a2766d8b36feeab49005e9472021a1d6
nadsbuhdha <nadsbuhdha@gmail.com>
2021-11-06 19:24:16 +0000

    addition question added

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,25 @@ document.addEventListener('DOMContentLoaded', function(){
                 alert('You Clicked Submit');
             } else {
                 let gameType = this.getAttribute('data-type');
-                alert(`You clicked ${gameType}`)
+                runGame(gameType);
             }
         })
     }
+    runGame('addition');
 })
 
 /**main game loop, called when the script is first loaded */
-function runGame() {
+function runGame(gameType) {
 
-let num1 = Math.floor(Math.random() *25 +1);
-let num2 = Math.floor(Math.random() *25 +1);
+let num1 = Math.floor(Math.random() * 25 + 1);
+let num2 = Math.floor(Math.random() * 25 + 1);
+ 
+if (gameType === 'addition') {
+    displayAdditionQuestion(num1, num2);
+} else {
+    alert (`Unknow game type: ${gameType}`);
+    throw `Unknow game type: ${gameType}.Aborting!`
+}
 }
 
 function checkAnswer() {
@@ -31,7 +39,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion (operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 
